1a05b4e21d1888445421eb23190c382279faad72
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-14 10:16:13 -0700

    Update task_1.py

--- homework/task_1.py
+++ homework/task_1.py
@@ -12,23 +12,34 @@
 
 # 3. Use the np.linalg.eig function to calculate the eigenvalues and eigenvectors of A.
 #    - Save the eigenvalues in a variable called eigenvalues.
-#    - Save the eigenvectors in a variable called eigenvectors.
+#    - Save the eigenvectors_as_cols in a variable called eigenvectors.
 
 
 
-# 4. Print:
+# 4. Use a list comprehension to convert eigenvectors_as_cols to a list of eigenvectors.
+# Save the output to a variable called eigenvectors.
+
+
+
+# 5. Print:
 # Eigenvalues: ###
 # where ### are the eigenvalues from above.
 
 
 
-# 5. Print:
-# Eigenvectors: ###
-# where ### are the eigenvectors from above.
+# 6. Print:
+# Eigenvectors as columns: ###
+# where ### is the eigenvectors_as_cols variable from above.
 
 
 
-# 6. Create a function called check_eigensystem that verifies whether a set of vectors and a set of eigenvalues
+# 7. Print:
+# List of eigenvectors: ###
+# where ### is the list of eigenvectors you created.
+
+
+
+# 8. Create a function called check_eigensystem that verifies whether a set of vectors and a set of eigenvalues
 # represents the eigensystem of A.
 #    - Parameters:
 #      - A: The matrix for which the eigenvector is being checked.
@@ -37,20 +48,24 @@
 
 
 
-    # 7. Use a loop and the zip function to iterate through each eigenvalue-eigenvector pair.
+    # 9. Use a loop and the zip function to iterate through each eigenvalue-eigenvector pair.
 
 
 
-        # 8. Use numpy's allclose comparison operator to check if the pair
+        # 10. Use numpy's allclose comparison operator to check if the pair
         # is indeed an eigenvalue-eigenvector pair of A. If it is not, print
         # the message:
         # Not a valid eigenvalue-eigenvector pair of #: ##, ###
         # Where # is A, ## is the eigenvalue, and ### is the eigenvector.
         # And return the boolean False.
 
-    # 9. If all pairs pass the eigenvalue-eigenvector tests for A, return True.
 
 
-# 10. Apply the check_eigenvector function to the matrix, eigenvectors, and eigenvalues from 3.
+    # 11. If all pairs pass the eigenvalue-eigenvector tests for A, return True.
+
+
+
+
+# 12. Apply the check_eigensystem function to the matrix, eigenvectors, and eigenvalues from 5 & 7.
 
 
